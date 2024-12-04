bd8965884a6f1bdfa3f3c9b8cfc25246054ee530
Luis Flor Chacón <luisflorchacon7@gmail.com>
2024-12-04 11:58:45 +0000

    Añadir tarear con el enter y cambio de border al estar el cursor encima

--- src/pages/ToDoList.jsx
+++ src/pages/ToDoList.jsx
@@ -4,16 +4,16 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import { useState } from "react";
 
-const listaInicial = [
-  {
-    tarea: "Hacer ejercicio",
-    id: crypto.randomUUID(),
-  },
-];
-
 export const TareasLista = () => {
+  const listaInicial = [
+    {
+      tarea: "Hacer ejercicio",
+      id: crypto.randomUUID(),
+    },
+  ];
   const [lista, setLista] = useState(listaInicial);
   const [nuevaTarea, setNuevaTarea] = useState("");
+  const [cursorEncima, setCursorEncima] = useState(null);
 
   const borrarTarea = (id) => {
     setLista(
@@ -30,6 +30,12 @@ export const TareasLista = () => {
     setNuevaTarea("");
   };
 
+  const manejarTecla = (e) => {
+    if (e.key === "Enter") {
+      agregarTarea();
+    }
+  };
+
   return (
     <>
       <div className="d-flex justify-content-center">
@@ -38,8 +44,9 @@ export const TareasLista = () => {
             placeholder="Añade tus tareas cotidianas"
             value={nuevaTarea}
             onChange={(e) => setNuevaTarea(e.target.value)}
+            onKeyUp={manejarTecla}
           />
-          <Button variant="outline-secondary" onClick={agregarTarea}>
+          <Button variant="outline-primary" onClick={agregarTarea}>
             Añadir tarea
           </Button>
         </InputGroup>
@@ -47,16 +54,34 @@ export const TareasLista = () => {
       {lista.map((Element) => {
         return (
           <>
-            <div className="d-flex justify-content-center">
+            <div
+              className="d-flex justify-content-center"
+              onMouseEnter={() => setCursorEncima(Element.id)}
+              onMouseLeave={() => setCursorEncima(null)}
+            >
               <div
-                className="m-2 border border-2 border-info rounded d-flex justify-content-between"
-                style={{ width: "600px" }}
+                className="m-2 rounded d-flex justify-content-between"
+                style={{
+                  width: "600px",
+                  border:
+                    cursorEncima === Element.id
+                      ? "2px solid blue"
+                      : "1px solid grey",
+                }}
                 key={Element.id}
+                onMouseEnter={() => setCursorEncima(Element.id)}
+                onMouseLeave={() => setCursorEncima(null)}
               >
                 <p className="p-2">{Element.tarea}</p>
                 <strong
                   className="p-2 text-danger"
-                  style={{ cursor: "pointer" }}
+                  style={{
+                    cursor: "pointer",
+                    visibility:
+                      cursorEncima === Element.id ? "visible" : "hidden",
+                    pointerEvents:
+                      cursorEncima === Element.id ? "auto" : "none",
+                  }}
                   onClick={() => {
                     return borrarTarea(Element.id);
                   }}
